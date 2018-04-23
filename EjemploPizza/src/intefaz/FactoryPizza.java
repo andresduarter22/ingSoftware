008157234c590d0b063e0effd0ca195285283320
andres <andresduarter13@gmail.com>
2018-04-23 08:57:25 -0400

    switch

--- EjemploPizza/src/intefaz/FactoryPizza.java
+++ EjemploPizza/src/intefaz/FactoryPizza.java
@@ -3,7 +3,21 @@ package intefaz;
 public class FactoryPizza {
 	
 	public static IPizza make(String type) {
-		return null;
+		
+		IPizza pizza = null;
+		switch(type) {
+		case "carne":
+			pizza= new Carne();
+		break;
+		case "hawaiana":
+			pizza = new Hawaiana();
+		break;
+		case "tradicional":
+			pizza = new Tradicional();
+		break;
+		}
+		return pizza;
+		
 	}
 
 }
