3538c088feb975f536f347746d9102802c259b99
andres <andresduarter13@gmail.com>
2018-04-23 08:48:27 -0400

    main y factory

---
+++ EjemploPizza/src/intefaz/FactoryPizza.java
@@ -0,0 +1,5 @@
+package intefaz;
+
+public class FactoryPizza {
+
+}
---
+++ EjemploPizza/src/intefaz/Main.java
@@ -0,0 +1,10 @@
+package intefaz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
