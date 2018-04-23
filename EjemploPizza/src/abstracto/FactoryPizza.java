caf8bacf02bd6e3d5a227518bcd33114251ceec9
andres <andresduarter13@gmail.com>
2018-04-23 09:20:42 -0400

    listo

--- EjemploPizza/src/abstracto/FactoryPizza.java
+++ EjemploPizza/src/abstracto/FactoryPizza.java
@@ -1,10 +1,27 @@
 package abstracto;
 
+import intefaz.Carne;
+import intefaz.Hawaiana;
+import intefaz.Tradicional;
+
 public class FactoryPizza {
 	public static Pizza make(String type) {
 		
+	
+		Pizza pizza ;
 		
-		return null;
+		switch(type) {
+		case "carne":
+			pizza= new Carne();
+		break;
+		case "hawaiana":
+			pizza = new Hawaiana();
+		break;
+		case "tradicional":
+			pizza = new Tradicional();
+		break;
+		}
+		return pizza;
 		
 	}
 
